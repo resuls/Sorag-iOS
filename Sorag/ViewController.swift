4498b0a94a545291961a804ad8c161510091a69b
Resulkary Saparov <resulsaparov@gmail.com>
2020-02-06 15:50:56 +0100

    Initial Commit

---
+++ Sorag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sorag
+//
+//  Created by Resul on 06.02.20.
+//  Copyright © 2020 Resulkary Saparov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
